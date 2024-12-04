49735f7e2050d3d676042e976300ae87e8fd2dd4
Rafael Paniago <rafaelpaniago1@gmail.com>
2024-12-03 23:02:30 -0300

    Primeiros comandos de write

--- 01conceito/Program.cs
+++ 01conceito/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Comentário
+
+/*
+    Comentário 2
+*/
+
+
+Console.WriteLine("Hello world");
+Console.Write("Linha 2");
