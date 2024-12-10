0cddc76527e8eb82de77037c140389a109f0bec8
Rafael Paniago <rafaelpaniago1@gmail.com>
2024-12-09 23:14:16 -0300

    Arquivos adicionados

---
+++ 04concatenacao/Program.cs
@@ -0,0 +1,18 @@
+﻿// Variáveis
+
+string nome = "Rafael";
+string cidade = "São Paulo";
+
+// Concatenação através do +
+Console.WriteLine(nome+" mora em "+cidade);
+
+// Concatenação através de interpolação
+Console.WriteLine($"{nome} mora em {cidade}");
+
+
+
+
+
+
+
+
---
+++ 06conversao/Program.cs
@@ -0,0 +1,12 @@
+﻿// Obter o primeiro número
+Console.WriteLine("Informe um número: ");
+int numero1 = Convert.ToInt16(Console.ReadLine());
+
+// Obter o segundo número
+Console.WriteLine("Informe o segundo número: ");
+int numero2 = Convert.ToInt16(Console.ReadLine());
+
+// Imprimir os valores somados
+Console.WriteLine($"A soma é {numero1+numero2}");
+
+
