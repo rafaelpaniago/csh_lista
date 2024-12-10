59bcc2b7c71116af17b1f509b3cc44c60638c3d2
Rafael Paniago <rafaelpaniago1@gmail.com>
2024-12-09 22:35:10 -0300

    Nova pasta adicionada: 02variaveis

---
+++ 02variaveis/Program.cs
@@ -0,0 +1,29 @@
+﻿// TEXTOS
+string nome = "Rafael";
+
+
+// LETRAS
+char inicial = 'r';
+
+
+// NÚMEROS INTEIROS
+int idade = 34;
+
+
+// NÚMEROS REAIS (DECIMAL)
+double altura = 1.79;
+
+
+// LÓGICA
+bool estuda = true;
+
+
+// EXIBIR VALORES DAS VARIÁVEIS
+Console.WriteLine(nome);
+Console.WriteLine(inicial);
+Console.WriteLine(idade);
+Console.WriteLine(altura);
+Console.WriteLine(estuda);
+
+
+
