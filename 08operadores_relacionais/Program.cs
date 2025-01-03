59c919175bdb1be5bca33465089e0d5f5244424d
Rafael Paniago <rafaelpaniago1@gmail.com>
2025-01-03 17:53:18 -0300

    Operadores relacionais

---
+++ 08operadores_relacionais/Program.cs
@@ -0,0 +1,24 @@
+﻿// Variáveis
+
+int num01 = 5;
+int num02 = 8;
+
+// MAIOR
+// Quando faço um comparativo o retorno é true ou false
+Console.WriteLine(num01 > num02);
+
+// MAIOR OU IGUAL
+Console.WriteLine(num02 >= num01);
+
+// MENOR 
+Console.WriteLine(num01 < num02);
+
+// MENOR OU IGUAL
+Console.WriteLine(num02 <= num01);
+
+// IGUAL
+Console.WriteLine(num01 == num02);
+
+// DIFERENTE
+Console.WriteLine(num01 != num02);
+
