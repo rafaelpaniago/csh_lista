8349cf568914ba38966c62b04f724cdd252110ed
Rafael Paniago <rafaelpaniago1@gmail.com>
2025-01-03 17:41:07 -0300

    Operadores aritimeticos

---
+++ 07operadores_artimeticos/Program.cs
@@ -0,0 +1,27 @@
+﻿// Variáveis
+
+int numero01 = 30;
+int numero02 = 3;
+
+// Divisão
+Console.WriteLine(numero01/numero02);
+
+// Multiplicação
+Console.WriteLine(numero01*numero02);
+
+// Subtração
+Console.WriteLine(numero01-numero02);
+
+// Soma
+Console.WriteLine(numero01+numero02);
+
+// Módulo (resto da divisão)
+Console.WriteLine(numero01%numero02);
+
+// Incrementador
+numero01++; // Pego meu valor 30 e acrescento + 1, retornando 31
+Console.WriteLine(numero01);
+
+// Decrementador
+numero02--; // Pego meu valor 3 e tiro 1, retornando 2
+Console.WriteLine(numero02);
